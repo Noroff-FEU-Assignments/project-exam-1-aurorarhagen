5684ff934f21f965b266ebb059799ef1aaeb574f
aurorarhagen <133155758+aurorarhagen@users.noreply.github.com>
2024-02-16 20:44:37 +0100

    Create form.js

---
+++ js/form.js
@@ -0,0 +1,11 @@
+             //* Form validation //*
+
+// Name - min 5 characters 
+
+// Valid e-mail adress
+
+// Subject - min 15 characters
+
+// Message - min 25 characters 
+
+// Error messages 
